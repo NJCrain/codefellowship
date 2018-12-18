000fb973ed21dc9cb6ea3a19ff656efbeecffce4
Nick Crain <nicholascrain@gmail.com>
2018-12-18 10:31:28 -0800

    minor changes

--- src/main/java/com/njcrain/codefellowship/controllers/HomeController.java
+++ src/main/java/com/njcrain/codefellowship/controllers/HomeController.java
@@ -1,4 +1,4 @@
-package com.njcrain.codefellowship;
+package com.njcrain.codefellowship.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
