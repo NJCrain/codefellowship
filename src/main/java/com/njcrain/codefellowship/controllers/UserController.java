df5eb8d340462b815d8bbc07563a5f681081983c
Nick Crain <nicholascrain@gmail.com>
2018-12-19 11:06:24 -0800

    successful login takes you to myprofile

--- src/main/java/com/njcrain/codefellowship/controllers/UserController.java
+++ src/main/java/com/njcrain/codefellowship/controllers/UserController.java
@@ -3,10 +3,13 @@ package com.njcrain.codefellowship.controllers;
 import com.njcrain.codefellowship.ApplicationUser;
 import com.njcrain.codefellowship.ApplicationUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import java.security.Principal;
 
 @Controller
 public class UserController {
@@ -18,6 +21,12 @@ public class UserController {
         ApplicationUser user = applicationUserRepo.findById(id).get();
         m.addAttribute("title", user.getUsername());
         m.addAttribute("user", user);
+        return "user";
+    }
+
+    @GetMapping("/myprofile")
+    public String showProfile(Principal p, Model m) {
+        m.addAttribute("user", ((UsernamePasswordAuthenticationToken) p).getPrincipal());
         return "user";
     }
 }
