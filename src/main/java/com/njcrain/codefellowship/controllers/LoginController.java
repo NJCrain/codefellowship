0ea4c760b2178122de0af0300bdd6de0ddaa6f66
Nick Crain <nicholascrain@gmail.com>
2018-12-20 14:44:54 -0800

    dynamic navbar added

--- src/main/java/com/njcrain/codefellowship/controllers/LoginController.java
+++ src/main/java/com/njcrain/codefellowship/controllers/LoginController.java
@@ -1,15 +1,24 @@
 package com.njcrain.codefellowship.controllers;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
 
 @Controller
 public class LoginController {
 
     @GetMapping("/login")
-    public String show(Model m) {
+    public String show(Model m, Principal p) {
         m.addAttribute("title", "Login");
+        if (p == null) {
+            m.addAttribute("loggedIn", false);
+        } else {
+            m.addAttribute("loggedIn", true);
+            m.addAttribute("user", ((UsernamePasswordAuthenticationToken) p).getPrincipal());
+        }
         return "login";
     }
 }
